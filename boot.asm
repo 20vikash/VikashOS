7f8a2e70d75e9869fdd21df214d5adb14acadeaa
Vikash <callofdutyvikash@gmail.com>
2025-05-10 10:03:33 +0530

    Switch to protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 start_:
     jmp short start
@@ -8,58 +11,64 @@ start_:
 times 33 db 0
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti
 
-    mov ah, 2
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    ; dl is already set
-    mov bx, buffer
-    int 0x13
-
-    jc error
-
-    mov si, buffer
-    call print
-
     jmp $
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-error:
-    mov si, error_message
-    call print
+gdt_start:
+gdt_null:
+    dd 0
+    dd 0
+
+gdt_code:       ;CS should point to this
+    dw 0xffff   ;Segment limit first 0-15 bits
+    dw 0        ;Base first 0-15 bits
+    db 0        ;Base 16-23 bits
+    db 0x9a     ;Access byte
+    db 11001111b    ;High 4 bit flags and low 4 bit flags
+    db 0        ;Base 24-31 bits
+
+gdt_data:       ;DS should point to this
+    dw 0xffff   ;Segment limit first 0-15 bits
+    dw 0        ;Base first 0-15 bits
+    db 0        ;Base 16-23 bits
+    db 0x92     ;Access byte
+    db 11001111b    ;High 4 bit flags and low 4 bit flags
+    db 0        ;Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
-
-buffer:
