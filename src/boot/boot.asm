ed3eba2e8fc5271307fd3d3f6b4bf86ac1f661e7
Vikash <callofdutyvikash@gmail.com>
2025-05-10 11:34:05 +0530

    Enable the A20 line for more than 1MB addressing

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -68,6 +68,12 @@ load32:
     mov ss, ax
     mov ebp, 0x00200000
     mov esp, ebp
+
+    ; Enable the A20 line
+    mov al, 0x92 ; Port number for the a20 fast chipset
+    or al, 2
+    out 0x92, al
+
     jmp $
 
 times 510 - ($ - $$) db 0
