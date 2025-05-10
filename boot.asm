8a775f3d739cf7abf5a320aeb0885e33b8e07360
Vikash <callofdutyvikash@gmail.com>
2025-05-10 06:05:41 +0530

    Custom interrupts for testing

--- boot.asm
+++ boot.asm
@@ -7,6 +7,13 @@ start_:
 
 times 33 db 0
 
+; handle_zero:
+;     mov ah, 0eh
+;     mov bx, 0
+;     mov al, 'V'
+;     int 0x10
+;     iret
+
 start:
     jmp 0x7c0:step2
 
@@ -19,6 +26,14 @@ step2:
     mov ss, ax
     mov sp, 0x7c00
     sti
+
+    ; Interrupt starts from the 0x00 of the RAM and its typically the division by 0 error.
+    ; Real mode interrupts are set by the BIOS, and protected mode interrupts are different from this.
+    ; mov word[ss:0x00], handle_zero ; The size of the interrupt record is 4 bytes.
+    ; mov word[ss:0x02], 0x7c0
+    ; handle_zero:0x7c0 = offset:segment
+
+    ; int 0
 
     mov si, message
     call print
