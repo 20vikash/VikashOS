d94ec502987b43a28e0da7be9c8cb160b290fa35
Vikash <callofdutyvikash@gmail.com>
2025-05-12 19:24:39 +0530

    Change char to uint16 to give the whole 2 bytes in one go

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,9 @@
 #include "kernel.h"
+#include <stdint.h>
 
 void kernel_main() 
 {
-    char* video_memory = (char*)(0xB8000);
+    uint16_t* video_memory = (uint16_t*)(0xB8000);
 
-    video_memory[0] = 'A';
-    video_memory[1] = 2;
-    video_memory[2] = 'C';
-    video_memory[3] = 2;
+    video_memory[0] = 0x0341; // The first byte is the ascii, 2nd byte is the color. (little endian)
 }
