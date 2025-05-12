c9f59f323ce1004c3796a68122a5d05395ec4aec
Vikash <callofdutyvikash@gmail.com>
2025-05-12 20:23:02 +0530

    Write printf routine that accepts characters and newline escape character

--- src/kernel.c
+++ src/kernel.c
@@ -3,19 +3,11 @@
 #include <stddef.h>
 
 uint16_t* video_memory = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_column = 0;
 
 uint16_t terminal_make_char(char c, char color) {
     return (color << 8) | c; // The first byte is the ascii, 2nd byte is the color. (little endian)
-}
-
-void initialize_terminal() {
-    video_memory = (uint16_t*)(0xB8000);
-
-    for (int y = 0; y < VGA_HEIGHT; y++) {
-        for (int x = 0; x < VGA_WIDTH; x++) {
-            video_memory[(y * VGA_WIDTH) + x] = terminal_make_char(' ', 0);
-        }
-    }
 }
 
 size_t strlen(const char *str) {
@@ -28,8 +20,49 @@ size_t strlen(const char *str) {
     return len;
 }
 
+void terminal_put_char(int x, int y, char c, char color) {
+    video_memory[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+void terminal_write_char(char c, char color) {
+    if (c == '\n') {
+        terminal_column = 0;
+        terminal_row++;
+        return;
+    }
+
+    terminal_put_char(terminal_column, terminal_row, c, color);
+
+    terminal_column++;
+    if (terminal_column % VGA_WIDTH == 0) {
+        terminal_column = 0;
+        terminal_row++;
+    }
+}
+
+void printf(char *c) {
+    size_t len = strlen(c);
+
+    for (int i = 0; i < len; i++)
+    {
+        terminal_write_char(c[i], 15);
+    }
+}
+
+void initialize_terminal() {
+    video_memory = (uint16_t*)(0xB8000);
+    terminal_row = 0;
+    terminal_column = 0;
+
+    for (int y = 0; y < VGA_HEIGHT; y++) {
+        for (int x = 0; x < VGA_WIDTH; x++) {
+            terminal_put_char(x, y, ' ', 0);
+        }
+    }
+}
+
 void kernel_main() 
 {
     initialize_terminal();
-    video_memory[0] = terminal_make_char('b', 15);
+    printf("Hello world. \nMy name is Vikash");
 }
