7f36278aba2b5daffb63bc4e601389d028cea508
Vikash <callofdutyvikash@gmail.com>
2025-05-12 19:33:50 +0530

    Make the final hex value from the passed character and the color code

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,10 @@
 #include "kernel.h"
 #include <stdint.h>
 
+uint16_t terminal_make_char(char c, char color) {
+    return (color << 8) | c;
+}
+
 void kernel_main() 
 {
     uint16_t* video_memory = (uint16_t*)(0xB8000);
