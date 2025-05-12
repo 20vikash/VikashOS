d10251cca8bb6f9acf9ffc989bea12abc41ef743
Vikash <callofdutyvikash@gmail.com>
2025-05-12 19:47:32 +0530

    Clear out the terminal

--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,23 @@
 #include "kernel.h"
-#include <stdint.h>
+
+uint16_t* video_memory = 0;
 
 uint16_t terminal_make_char(char c, char color) {
-    return (color << 8) | c;
+    return (color << 8) | c; // The first byte is the ascii, 2nd byte is the color. (little endian)
+}
+
+void initialize_terminal() {
+    video_memory = (uint16_t*)(0xB8000);
+
+    for (int y = 0; y < VGA_HEIGHT; y++) {
+        for (int x = 0; x < VGA_WIDTH; x++) {
+            video_memory[(y * VGA_WIDTH) + x] = terminal_make_char(' ', 0);
+        }
+    }
 }
 
 void kernel_main() 
 {
-    uint16_t* video_memory = (uint16_t*)(0xB8000);
-
-    video_memory[0] = 0x0341; // The first byte is the ascii, 2nd byte is the color. (little endian)
+    initialize_terminal();
+    video_memory[0] = terminal_make_char('b', 3);
 }
