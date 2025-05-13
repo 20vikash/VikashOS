ec402b2d6abdf54ff5cdf9d111b987bec9c34dd6
Vikash <callofdutyvikash@gmail.com>
2025-05-13 14:36:21 +0530

    Write insb routine which is compatable with gcc function call

---
+++ src/io/io.asm
@@ -0,0 +1,14 @@
+section .asm
+
+global insb
+
+insb: ; Read one byte from the dx port.
+    push ebp
+    mov ebp, esp
+
+    xor eax, eax ; Clear out eax (ABI says to have the return value in eax)
+    mov edx, [ebp+8]
+    in al, dx
+
+    pop ebp
+    ret
