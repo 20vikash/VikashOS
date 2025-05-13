99b73a40ead79dc54c1eceaea29c976d3b07d833
Vikash <callofdutyvikash@gmail.com>
2025-05-13 14:47:13 +0530

    outb and outw

--- src/io/io.asm
+++ src/io/io.asm
@@ -1,6 +1,9 @@
 section .asm
 
 global insb
+global insw
+global outb
+global outw
 
 insb: ; Read one byte from the dx port.
     push ebp
@@ -20,6 +23,28 @@ insw:
     xor eax, eax
     mov edx, [ebp+8]
     in ax, dx
+
+    pop ebp
+    ret
+
+outb:
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp+12]
+    mov edx, [ebp+8]
+    out dx, al
+
+    pop ebp
+    ret
+
+outw:
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp+12]
+    mov edx, [ebp+8]
+    out dx, ax
 
     pop ebp
     ret
