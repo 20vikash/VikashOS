7e1cb888b96d4108aaeb24d4ce42e02812166a92
Vikash <callofdutyvikash@gmail.com>
2025-05-30 20:50:37 +0530

    Validate address allocation alignment

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -1,5 +1,13 @@
 #include "heap.h"
+#include "kernel.h"
+#include <stdbool.h>
+
+static bool heap_validate_alignment(void* ptr) {
+    return ((unsigned int)ptr % VIKASHOS_HEAP_BLOCK_SIZE) == 0;
+}
 
 int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* table) {
-    return 0;
+    int res = 0;
+
+    return res;
 }
