6b2e6dee03299ca8454a0b5ef873fc8f1c236e16
Vikash <callofdutyvikash@gmail.com>
2025-05-31 09:39:22 +0530

    Mark the taken blocks as taken in the heap

--- src/memory/heap/heap.h
+++ src/memory/heap/heap.h
@@ -10,7 +10,7 @@
 
 // Last 4 bits of the entry (4-7)
 #define HEAP_BLOCK_HAS_NEXT 0b10000000
-#define HEAP_BLOCK_IS_FREE  0b01000000
+#define HEAP_BLOCK_IS_FIRST  0b01000000
 
 typedef unsigned char HEAP_BLOCK_TABLE_ENTRY; // 8 bits (1 entry)
 
