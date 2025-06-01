17d61acba4c07a84607cb272641b749da4080140
Vikash <callofdutyvikash@gmail.com>
2025-06-01 18:15:46 +0530

    Another paging test

--- src/kernel.c
+++ src/kernel.c
@@ -79,11 +79,12 @@ void kernel_main()
     kernel_chunk = paging_new_4gb(PAGING_ACCESS_FROM_ALL | PAGING_IS_WRITABLE | PAGING_IS_PRESENT);
     // Switch to kernel paging chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
-    // Enable paging
-    enable_paging();
 
     char* ptr = kzalloc(4096);
     paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_IS_PRESENT | PAGING_IS_WRITABLE | PAGING_ACCESS_FROM_ALL);
+
+    // Enable paging
+    enable_paging();
 
     char *ptr2 = (char *)0x1000;
     ptr2[0] = 'A';
