7bd2e8c31a260a18e8738e1fda4343517c65d793
Vikash <callofdutyvikash@gmail.com>
2025-06-01 13:09:06 +0530

    Enable paging in CPU by setting its CR0's 31st bit to 1

--- src/memory/paging/paging.asm
+++ src/memory/paging/paging.asm
@@ -3,6 +3,7 @@
 section .asm
 
 global paging_load_directory
+global enable_paging
 
 paging_load_directory:
     push ebp
@@ -10,6 +11,17 @@ paging_load_directory:
     mov eax, [ebp + 8]
 
     mov cr3, eax ; CR3 should contain the address to the page directory.
+
+    pop ebp
+    ret
+
+enable_paging:
+    push ebp
+    mov ebp, esp
+
+    mov eax, cr0
+    mov eax, 0x80000000 // Apply the 31st bit to enable paging
+    mov cr0, eax
 
     pop ebp
     ret
