bb40e58399db1cbc94a82a7f72c65019623cf603
Vikash <callofdutyvikash@gmail.com>
2025-06-02 09:54:22 +0530

    Commands to the ATA controller to make it ready for reading from the given lba

--- src/disk/disk.c
+++ src/disk/disk.c
@@ -1,4 +1,12 @@
+#include "io/io.h"
 
 int read_disk_sector(int lba, int total, void* buf) {
+    outb(0x1F6, (lba >> 24) | 0xE0);
+    outb(0x1F2, total);
+    outb(0x1F3, (unsigned char)(lba && 0xff));
+    outb(0x1F4, (unsigned char)(lba >> 8));
+    outb(0x1F5, (unsigned char)(lba >> 16));
+    outb(0x1F7, 0x20);
+
     return 0;
 }
---
+++ src/disk/disk.h
@@ -0,0 +1,6 @@
+#ifndef DISK_H
+#define DISK_H
+
+int read_disk_sector(int lba, int total, void* buf);
+
+#endif
